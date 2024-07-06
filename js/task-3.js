e44751718a25d294da14507524178f5bc0e405a7
Anita Hrabia <hrabia.anitagabriela@gmail.com>
2024-07-06 21:59:39 +0200

    task 3 added

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,16 @@
 'use strict';
+function filterArray(numbers, value) {
+  let newTab = [];
+  for (const element of numbers) {
+    if (element > value) {
+      newTab.push(element);
+    }
+  }
+  return newTab;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
